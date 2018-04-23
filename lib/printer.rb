92b5ded2775ca27664fcdfa22be6dfccaa229b44
danielcampos <ddregalo@gmail.com>
2018-04-23 18:52:50 +0100

    TDD Printer class - test passed

---
+++ lib/printer.rb
@@ -0,0 +1,10 @@
+# Printer class puts an account transaction history to console STDOUT
+class Printer
+  def print_statement(transaction_history)
+    account_statement = ['DATE || CREDIT || DEBIT || BALANCE']
+    transaction_history.each do |transaction|
+      account_statement << transaction
+    end
+    puts account_statement.join("\n")
+  end
+end
